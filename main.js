7cfc000970da640ba3c848a528ca4e2cda4f625d
bobnearents <bobnearents@yahoo.com>
2019-01-25 09:49:38 -0800

    finished step 2

--- main.js
+++ main.js
@@ -3,9 +3,23 @@
 
 function getRandomDog() {
   const input = getInputVal();
+  
   fetch(`https://dog.ceo/api/breeds/image/random/${input}`)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then((responseJson) => {
+      console.log(responseJson.message);
+      return display(generateString(responseJson.message));
+    });
+}
+
+function generateString(promiseMessage) {
+  let htmlString = '';
+  promiseMessage.forEach(item => htmlString += `<li><img src = "${item}"></img></li>`);
+  return htmlString;
+}
+
+function display(string) {
+  $('ul').html(string);
 }
 
 function getInputVal() {
